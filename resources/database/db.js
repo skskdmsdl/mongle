97751e153b8e838301739d1c2e17935e5d748c57
skskdmsdl <skskdmsdl@gmail.com>
2024-11-18 22:21:54 +0900

    create notice table

--- resources/database/db.js
+++ resources/database/db.js
@@ -2,3 +2,13 @@ import Database from 'better-sqlite3';
 
 // 데이터베이스 파일 생성 및 연결
 const db = new Database('database.db');
+
+// `notice` 테이블 생성
+db.exec(`
+    CREATE TABLE IF NOT EXISTS notice (
+      notice_seq INTEGER PRIMARY KEY AUTOINCREMENT, -- 자동 증가하는 기본 키
+      title TEXT NOT NULL,                          -- 제목
+      content TEXT NOT NULL,                        -- 내용
+      created_date TEXT NOT NULL                    -- 생성 날짜
+    )
+`);
