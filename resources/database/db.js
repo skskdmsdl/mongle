1533f32e626ab5be790c6014dace72c29e476177
skskdmsdl <skskdmsdl@gmail.com>
2024-11-19 23:02:53 +0900

    주석 수정

--- resources/database/db.js
+++ resources/database/db.js
@@ -1,12 +1,12 @@
-import Database from 'better-sqlite3';
+import Database from "better-sqlite3";
 
 // 데이터베이스 파일 생성 및 연결
-const db = new Database('database.db');
+const db = new Database("database.db");
 
 // `notice` 테이블 생성
 db.exec(`
     CREATE TABLE IF NOT EXISTS notice (
-      notice_seq INTEGER PRIMARY KEY AUTOINCREMENT, -- 자동 증가하는 기본 키
+      notice_seq INTEGER PRIMARY KEY AUTOINCREMENT, -- 기본 키
       title TEXT NOT NULL,                          -- 제목
       content TEXT NOT NULL,                        -- 내용
       created_date TEXT NOT NULL                    -- 생성 날짜
